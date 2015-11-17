564e7bead20dda7bd23ec5abc34b00fe2674456e
Sergii Stets <sergeystets@gmail.com>
2015-11-17 20:51:22 +0200

    Added code of the main feature to main method of the Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,9 @@
  *         Date 15.11.2015
  */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main feature goes here...");
+        System.out.println("Some other code");
+    }
 }
