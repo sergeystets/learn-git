92f9472dc888620c681a8721c1ff58ad0a17b2e5
Sergii Stets <sergeystets@gmail.com>
2015-12-05 16:40:39 +0200

    Added costructor to person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,4 +7,12 @@
 public class Person {
 
     private String fullName;
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
