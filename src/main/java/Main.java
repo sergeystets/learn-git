a7f7d55bbfee1abf5e6ae4640a23c5a93bf8be74
Sergii Stets <sergeystets@gmail.com>
2015-12-05 16:13:21 +0200

    added mocks, waiting future functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Experimental code goes here...");
+        // waiting for a person class here
     }
 }
